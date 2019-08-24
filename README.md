8cba39e08a61d8a938d229eea5e1f5cf230c175c
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-08-24 10:43:11 -0400

    update README

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # svg-to-font
+
+## 1. Add Icons
+
+Create a folder named "icons" and move your svgs there
+
+## 2. Create Font
+```
+npm run build
+```
--- index.js
+++ index.js
@@ -6,13 +6,13 @@ async function run() {
   const fs = require("fs");
   const svg2ttf = require("svg2ttf");
 
+  if (!fs.existsSync("icons")) fs.mkdirSync("fonts");
+  if (!fs.existsSync("fonts")) fs.mkdirSync("fonts");
+
   const ttf = svg2ttf(fs.readFileSync("fonts/hello.svg", "utf8"), {});
   fs.writeFileSync("fonts/myfont.ttf", Buffer.from(ttf.buffer));
 
   const base64Font = fs.readFileSync(`fonts/myfont.ttf`).toString("base64");
-
-  console.log(base64Font);
-  console.log(base64Font.length);
 }
 
 run();
